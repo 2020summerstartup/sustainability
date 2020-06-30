28d7c4fa08f1ae17fc091b024bd8a787a3cd21a3
Amy Qian <aqian@g.hmc.edu>
2020-06-30 16:10:45 -0700

    Navbar functions now!

---
+++ src/components/FontAwesomeIcons/index.js
@@ -0,0 +1,14 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// import your icons
+import { faLeaf, faUser, faTrophy, faInfo, faHome, faHeart } from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+    faLeaf, 
+    faUser, 
+    faTrophy, 
+    faInfo,
+    faHome,
+    faHeart,
+);
