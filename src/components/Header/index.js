007eaf0611c522ca8bf1d0735e5a7170ca2f2e53
Amy Qian <aqian@g.hmc.edu>
2020-06-30 18:46:55 -0700

    header in progress...

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// import your fontawesome library
+import "../FontAwesomeIcons";
+// import when you need to use icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import "./index.css";
+
+const Header = () => (
+  <header>
+    <div className="title">
+      GO GREEN or GO HOME <FontAwesomeIcon icon="leaf" />
+    </div>
+  </header>
+);
+
+export default Header;
