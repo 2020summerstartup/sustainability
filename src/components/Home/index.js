b9e4811b9a89c12a8782f2514e14559562d82926
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-02 17:33:40 -0400

    Added total point counter (only updates on reload)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Counter from './counter';
  
 import { withAuthorization } from '../Session';
- 
+
+// Text to display on the homepage
 const HomePage = () => (
   <div className="base-container">
     <script>
@@ -11,11 +12,13 @@ const HomePage = () => (
     <h1>Home Sweet Home</h1>
     <h3>Track your sustainable actions here!</h3>
     <span role="img" aria-label="recycle">♻️</span>
-    You have earned a total of XXX points for your sustainable actions. Thank you!
+    {/* Total points earned. */}
+    You have earned a total of { parseInt(localStorage.getItem('waterBottle')) + parseInt(localStorage.getItem('cmontWalk')) } points for your sustainable actions. Thank you!
+    {/* Individual sustainable actions. */}
     <h3><b>Recycle Water Bottle</b></h3>
     <center><Counter susAction={'waterBottle'}/></center>
     <h3><b>Walk to Claremont Village</b></h3>
-    <p>(Just not during the first two weeks on campus!)</p>
+    (Just not during the first two weeks on campus!)
     <center><Counter susAction={'cmontWalk'}/></center>
   </div>
 );
