b6a84057afc8e1003736fb0986668a979c8c476c
linnda <2441756049@qq.com>
2020-07-02 00:26:09 -0400

    adds local storage

--- src/components/Home/counter.js
+++ src/components/Home/counter.js
@@ -4,23 +4,29 @@ import firebase from "../Firebase"
 var last_point;
 
 class Counter extends Component{
+    userData;
     constructor(props){
         super(props)
+        this.handleChange = this.handleChange.bind(this);
         this.state = {
-            count: 0
+            count: 0,
         };
     }
 
-    recycle = () =>{
+    recycle = (e) =>{
         this.setState(prevState => ({
             count: prevState.count + 10
           }));
+        e.preventDefault()
+        localStorage.setItem("data", JSON.stringify({count: this.state.count + 10,}))
     };
 
-    walk = () =>{
+    walk = (e) =>{
         this.setState(prevState => ({
             count: prevState.count + 15
           }));
+        e.preventDefault()
+        localStorage.setItem("data", JSON.stringify({count: this.state.count + 15,}))
     };
 
     decrement = () =>{
@@ -35,12 +41,28 @@ class Counter extends Component{
             })
         }
     };
+    handleChange = (e) => {
+        this.setState({[e.target.name]:e.target.name})
+    }
+    componentDidMount() {
+        this.userData = JSON.parse(localStorage.getItem('data'));
+     
+        if (localStorage.getItem('data')) {
+            this.setState({
+                count: this.userData.count
+        })
+    } else {
+        this.setState({
+            count: 0,
+        })
+    }
+    }
     render(){
     return (
         <div>
             <p>You have earned a total of {this.state.count} points!</p>
-            <button className='buzzButton' onClick={this.recycle}>Recycle Water Bottle</button>
-            <button className='buzzButton' onClick={this.walk}>Walk to Claremont Village</button>
+            <button className='buzzButton' onClick={this.recycle} onChange={this.handleChange}>Recycle Water Bottle</button>
+            <button className='buzzButton' onClick={this.walk} onChange={this.handleChange}>Walk to Claremont Village</button>
             <button className='undoButton' onClick={this.decrement}>Undo</button>
         </div>
         );
