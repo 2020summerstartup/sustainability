cb903daedbcc147da52a33cd38c3ebac56ce0b8f
Amy Qian <aqian@g.hmc.edu>
2020-07-01 23:29:22 -0700

    more styling dd2

--- src/components/Dropdown/Dropdown2.js
+++ src/components/Dropdown/Dropdown2.js
@@ -56,49 +56,26 @@ function Dropdown2() {
     option: (styles, state) => ({
       ...styles,
       color: state.isSelected ? "#FFF" : styles.color,
-      backgroundColor: state.isSelected ? '#00bfa6' : 'white',
+      backgroundColor: state.isSelected ? "#00bfa6" : "white",
       borderBottom: "1px solid rgba(0, 0, 0, 0.125)",
+    
       "&:hover": {
-        color: "#FFF",
-        backgroundColor: "#00bfa6"
-      }
+        color: state.isDisabled ? "grey" : "black",
+        backgroundColor: state.isDisabled ? "white" : "#00bfa6",
+      },
     }),
     control: (styles, state) => ({
       ...styles,
       boxShadow: state.isFocused ? "0 0 0 0.2rem #00bfa6)" : 0,
       borderColor: state.isFocused ? "#D0EAE2" : "#CED4DA",
+
+      cursor: state.isDisabled ? "not-allowed" : "default",
+
       "&:hover": {
-        borderColor: state.isFocused ? "#00bfa6" : "#00bfa6"
-      }
-    })
+        borderColor: state.isFocused ? "#00bfa6" : "#00bfa6",
+      },
+    }),
   };
-
-//   const brandColor = "#00bfa6";
-
-//   const customStyles = {
-//     // control: (styles) => ({
-//     control: (base, state) => ({
-//       ...base,
-//       boxShadow: state.isFocused ? 0 : 0,
-//       border: '2px solid lightgray',
-//       borderColor: state.isFocused ? brandColor : base.borderColor,
-//       "&:hover": {
-//         borderColor: state.isFocused ? brandColor : base.borderColor, 
-//         //   ...styles,
-//         //   backgroundColor: "white",
-//         //   "&:hover": { borderColor: "#00bfa6" },
-//         //   "&:focus": { borderColor: "#00bfa6" }, // border style on hover
-//         //   border: "2px solid lightgray", // default border color
-//         //   boxShadow: `0 0 0 1px '#00bfa6'`,
-//       },
-//     }),
-//     option: (provided, state) => ({
-//       ...provided,
-//       backgroundColor: state.hover ? "green" : "white",
-//       color: "black",
-//       // cursor: state.isDisabled ? 'not-allowed' : 'default',
-//     }),
-//   };
 
   return (
     <div>
@@ -114,6 +91,7 @@ function Dropdown2() {
       />
       <br />
       <b>Your dorm: {JSON.stringify(selectedValue, null, 2)}</b>
+      <h1></h1>
     </div>
   );
 }
