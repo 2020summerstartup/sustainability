e4a5be39674d034b262e0508045dca5c0dd0b678
Amy Qian <aqian@g.hmc.edu>
2020-07-03 17:03:01 -0700

    added static title header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 //import SignOutButton from "../SignOut";
+import Typography from '@material-ui/core/Typography';
 
 // import your fontawesome library
 import "../FontAwesomeIcons";
@@ -12,19 +13,29 @@ import "./index.css";
 import { AppBar, Toolbar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   header: {
-    background: '#00bfa6',
-    margin: '0',
-    padding: '0',
+    background: "#00bfa6",
+    height: "3rem",
   },
-});
+  title: {
+    marginTop: '0.25rem',
+    margin: '1rem',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: '7.5rem',
+    },
+  }
+}));
 
 const Header = () => {
   const classes = useStyles();
   return (
     <>
-      <AppBar position="static" className={classes.header}></AppBar>
+      <AppBar position="static" className={classes.header}>
+        <Typography variant="h6" className={classes.title} noWrap>
+          Sustainability Competition App
+        </Typography>
+      </AppBar>
     </>
   );
 };
