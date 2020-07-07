a5b6ca33ff0906cba930baec3212c6db29289443
Amy Qian <aqian@g.hmc.edu>
2020-07-07 15:25:45 -0700

    add icon to forget pw

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,59 +1,65 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
- 
-import { withFirebase } from '../Firebase';
-import * as ROUTES from '../../constants/routes';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
 import pwImg from "../../img/forgetpw.svg";
- 
+
+// import your fontawesome library
+import "../FontAwesomeIcons";
+// import when you need to use icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const PasswordForgetPage = () => (
   <div className="base-container">
     <h1>You forgot your password :(</h1>
     <div className="image">
-      <img alt='' src={pwImg} />
+      <img alt="" src={pwImg} />
     </div>
     <PasswordForgetForm />
   </div>
 );
- 
+
 const INITIAL_STATE = {
-  email: '',
+  email: "",
   error: null,
 };
- 
+
 class PasswordForgetFormBase extends Component {
   constructor(props) {
     super(props);
- 
+
     this.state = { ...INITIAL_STATE };
   }
- 
-  onSubmit = event => {
+
+  onSubmit = (event) => {
     const { email } = this.state;
- 
+
     this.props.firebase
       .doPasswordReset(email)
       .then(() => {
         this.setState({ ...INITIAL_STATE });
       })
-      .catch(error => {
+      .catch((error) => {
         this.setState({ error });
       });
- 
+
     event.preventDefault();
   };
- 
-  onChange = event => {
+
+  onChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   };
- 
+
   render() {
     const { email, error } = this.state;
- 
-    const isInvalid = email === '';
- 
+
+    const isInvalid = email === "";
+
     return (
       <form onSubmit={this.onSubmit} className="form">
         <div className="form-group">
+          <FontAwesomeIcon icon="envelope" className="icon envelope" />
           <input
             name="email"
             value={this.state.email}
@@ -65,21 +71,21 @@ class PasswordForgetFormBase extends Component {
         <button disabled={isInvalid} type="submit" className="button">
           Reset My Password
         </button>
- 
+
         {error && <p>{error.message}</p>}
       </form>
     );
   }
 }
- 
+
 const PasswordForgetLink = () => (
   <p>
     <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password? (rip)</Link>
   </p>
 );
- 
+
 export default PasswordForgetPage;
- 
+
 const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
- 
+
 export { PasswordForgetForm, PasswordForgetLink };
