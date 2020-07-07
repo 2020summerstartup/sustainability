63bf80f156b1ba0b8cf114d915d1f52ce3369b1d
Amy Qian <aqian@g.hmc.edu>
2020-07-06 20:45:01 -0700

    delete comments

--- src/components/Info/Rewards.js
+++ src/components/Info/Rewards.js
@@ -26,28 +26,3 @@ const goal = {
   padding: "1.5rem",
   borderRadius: "10px",
 };
-
-// import React, { Component } from "react";
-// import { Spring } from "react-spring/renderprops";
-
-// const goal = {
-//   background: "#00a8bf",
-//   color: "white",
-//   padding: "1.5rem",
-//   borderRadius: "10px",
-// };
-
-// export default function Rewards() {
-//   return (
-//     <Spring from={{ opacity: 0 }} from={{ opacity: 1 }}>
-//       {(props) => (
-//         <div style={props}>
-//           <div style={goal}>
-//             <h1>Current Rewards</h1>
-//             <p>Coming soon... :)</p>
-//           </div>
-//         </div>
-//       )}
-//     </Spring>
-//   );
-// }
