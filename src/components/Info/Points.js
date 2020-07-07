105b4ccddf38101603fc8aabb459ec714237dedf
Amy Qian <aqian@g.hmc.edu>
2020-07-06 20:43:23 -0700

    animate info cards

--- src/components/Info/Points.js
+++ src/components/Info/Points.js
@@ -1,28 +1,37 @@
 import React, { Component } from "react";
 import { Spring } from "react-spring/renderprops";
 
-const points = {
+export class Points extends Component {
+  render() {
+    return (
+      <Spring
+        from={{ opacity: 0, marginTop: -500 }}
+        to={{ opacity: 1, marginTop: 0 }}
+        config={{ delay: 1000, duration: 1000 }}
+      >
+        {(props) => (
+          <div style={props}>
+            <div style={c2Style}>
+              <h1>How to Participate & Earn Points</h1>
+              <p>
+                Sign up with your HMC gmail! Look on our actions page for //
+                sustainable practices that you can earn points for and hit the
+                // "BUZZ" button everytime you complete the action!
+              </p>
+            </div>
+          </div>
+        )}
+      </Spring>
+    );
+  }
+}
+
+const c2Style = {
   background: "#00bf96",
   color: "white",
   padding: "1.5rem",
   borderRadius: "10px",
 };
 
-export default function Points() {
-  return (
-    <Spring from={{ opacity: 0 }} from={{ opacity: 1 }}>
-      {(props) => (
-        <div style={props}>
-          <div style={points}>
-            <h1>How to Participate & Earn Points</h1>
-            <p>
-              Sign up with your HMC gmail! Look on our actions page for
-              sustainable practices that you can earn points for and hit the
-              "BUZZ" button everytime you complete the action!
-            </p>
-          </div>
-        </div>
-      )}
-    </Spring>
-  );
-}
+export default Points;
+
--- src/components/Info/Rewards.js
+++ src/components/Info/Rewards.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
 import { Spring } from "react-spring/renderprops";
 
-const goal = {
-  background: "#00a8bf",
-  color: "white",
-  padding: "1.5rem",
-  borderRadius: "10px",
-};
-
 export default function Rewards() {
   return (
-    <Spring from={{ opacity: 0 }} from={{ opacity: 1 }}>
+    <Spring
+      from={{ opacity: 0, marginTop: -500 }}
+      to={{ opacity: 1, marginTop: 0 }}
+      config={{ delay: 3000, duration: 1000 }}
+    >
       {(props) => (
         <div style={props}>
           <div style={goal}>
@@ -22,3 +19,35 @@ export default function Rewards() {
     </Spring>
   );
 }
+
+const goal = {
+  background: "#00a8bf",
+  color: "white",
+  padding: "1.5rem",
+  borderRadius: "10px",
+};
+
+// import React, { Component } from "react";
+// import { Spring } from "react-spring/renderprops";
+
+// const goal = {
+//   background: "#00a8bf",
+//   color: "white",
+//   padding: "1.5rem",
+//   borderRadius: "10px",
+// };
+
+// export default function Rewards() {
+//   return (
+//     <Spring from={{ opacity: 0 }} from={{ opacity: 1 }}>
+//       {(props) => (
+//         <div style={props}>
+//           <div style={goal}>
+//             <h1>Current Rewards</h1>
+//             <p>Coming soon... :)</p>
+//           </div>
+//         </div>
+//       )}
+//     </Spring>
+//   );
+// }
