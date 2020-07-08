33e60b03366773eb14b79a81584f5fc93fd04f19
Amy Qian <aqian@g.hmc.edu>
2020-07-08 11:18:14 -0700

    attempt form again

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,101 +1,88 @@
-import React from "react";
-{
-  /* <script type="text/babel"> */
-}
+import React, {useState, useCallback} from 'react';
+// import {useDropzone} from 'react-dropzone'
 
-const encode = (data) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-};
 
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "", email: "", message: "" };
+function App() {
+  const [name,setName] = useState('');
+  const [status,setStatus] = useState('');
+  const [email,setEmail] = useState('');
+  const [message,setMessage] = useState('');
+  const [file, setFile] = useState({});
+
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles)
+    setFile(acceptedFiles[0])
+  }, [])
+//   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  const encode = (data) => {
+    const formData = new FormData();
+    Object.keys(data).forEach((k)=>{
+      formData.append(k,data[k])
+    });
+    return formData
   }
 
-  /* Here’s the juicy bit for posting the form submission */
-
-  handleSubmit = (e) => {
+  const handleSubmit = e => {
+    const data = { "form-name": "contact", name, email, message, file }
+    
     fetch("/", {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state }),
+      // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
+      body: encode(data)
     })
-      .then(() => alert("Success!"))
-      .catch((error) => alert(error));
+      .then(() => setStatus("Form Submission Successful!!"))
+      .catch(error => setStatus("Form Submission Failed!"));
 
     e.preventDefault();
   };
 
-  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
-
-  render() {
-    const { name, email, message } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p>
-          <label>
-            Your Name:{" "}
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email:{" "}
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:{" "}
-            <textarea
-              name="message"
-              value={message}
-              onChange={this.handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    );
+  const handleChange = e => {
+    const {name, value} = e.target
+    if (name === 'name' ){
+      return setName(value)
+    }
+    if (name === 'email' ){
+      return setEmail(value)
+    }
+    if (name === 'message' ){
+      return setMessage(value)
+    }
   }
+
+  return (
+    <div className="App">
+    <form onSubmit={handleSubmit} action="/thank-you/">
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" value={message} onChange={handleChange} />
+            </label>
+          </p>
+          {/* <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+              isDragActive ?
+                <p>Drop the files here ...</p> :
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            }
+          </div> */}
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+        <h3>{status}</h3>
+    </div>
+  );
 }
 
-export default ContactForm;
-
-// </script>
-
-// import React from 'react';
-
-// const ContactForm = () => (
-//   <div>
-//     <form
-//       name="contact-form"
-//       method="post"
-//       data-netlify="true"
-//       data-netlify-honeypot="bot-field"
-//     >
-//       <input name="name" placeholder="Your Name" type="text" />
-//       <input name="email" placeholder="Email" type="email" />
-//       <textarea name="message" placeholder="Message"/>
-//       <button className="button" >Send</button>
-//       <p></p>
-//     </form>
-//   </div>
-// )
-
-// export default ContactForm
+export default App;
