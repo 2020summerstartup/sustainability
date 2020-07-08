b73d6a81908b4da986d9d7aec208e453f7fe8418
Amy Qian <aqian@g.hmc.edu>
2020-07-08 11:29:39 -0700

    still trying form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,88 +1,58 @@
-import React, {useState, useCallback} from 'react';
-// import {useDropzone} from 'react-dropzone'
+import React from 'react';
 
-
-function App() {
-  const [name,setName] = useState('');
-  const [status,setStatus] = useState('');
-  const [email,setEmail] = useState('');
-  const [message,setMessage] = useState('');
-  const [file, setFile] = useState({});
-
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
-    setFile(acceptedFiles[0])
-  }, [])
-//   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
-  const encode = (data) => {
-    const formData = new FormData();
-    Object.keys(data).forEach((k)=>{
-      formData.append(k,data[k])
-    });
-    return formData
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
   }
 
-  const handleSubmit = e => {
-    const data = { "form-name": "contact", name, email, message, file }
-    
-    fetch("/", {
-      method: "POST",
-      // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
-      body: encode(data)
-    })
-      .then(() => setStatus("Form Submission Successful!!"))
-      .catch(error => setStatus("Form Submission Failed!"));
-
-    e.preventDefault();
-  };
-
-  const handleChange = e => {
-    const {name, value} = e.target
-    if (name === 'name' ){
-      return setName(value)
+  class ContactForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { name: "", email: "", message: "" };
     }
-    if (name === 'email' ){
-      return setEmail(value)
-    }
-    if (name === 'message' ){
-      return setMessage(value)
-    }
-  }
 
-  return (
-    <div className="App">
-    <form onSubmit={handleSubmit} action="/thank-you/">
+    /* Here’s the juicy bit for posting the form submission */
+
+    handleSubmit = e => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+        .then(() => alert("Success!"))
+        .catch(error => alert(error));
+
+      e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+      const { name, email, message } = this.state;
+      return (
+        <form onSubmit={this.handleSubmit}>
           <p>
             <label>
-              Your Name: <input type="text" name="name" value={name} onChange={handleChange} />
+              Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
             </label>
           </p>
           <p>
             <label>
-              Your Email: <input type="email" name="email" value={email} onChange={handleChange} />
+              Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
             </label>
           </p>
           <p>
             <label>
-              Message: <textarea name="message" value={message} onChange={handleChange} />
+              Message: <textarea name="message" value={message} onChange={this.handleChange} />
             </label>
           </p>
-          {/* <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-              isDragActive ?
-                <p>Drop the files here ...</p> :
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            }
-          </div> */}
           <p>
             <button type="submit">Send</button>
           </p>
         </form>
-        <h3>{status}</h3>
-    </div>
-  );
-}
+      );
+    }
+  }
 
-export default App;
+export default ContactForm;
