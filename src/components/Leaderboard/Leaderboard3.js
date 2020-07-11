b46f1fbe10f658a5f3230dc42c69d333207d4943
Amy Qian <aqian@g.hmc.edu>
2020-07-11 14:48:34 -0700

    deleted comments

--- src/components/Leaderboard/Leaderboard3.js
+++ src/components/Leaderboard/Leaderboard3.js
@@ -44,27 +44,7 @@ class Leaderboard3 extends React.Component {
 			{ userId: 8, dorm: 'Linde', points: 120},
 			{ userId: 9, dorm: 'North', points: 100}
 		]), 500);
-		/*var socket = this.connect('wss://r1-contest-api.herokuapp.com', event => {
-			this.update(JSON.parse(event.data));
-		});*/
 	}
-
-	// connect(uri, messageHandler, tries) {
-	// 	tries = tries || 0;
-	// 	var socket = new WebSocket(uri);
-	// 	socket.addEventListener('open', event => {
-	// 		tries = 0;
-	// 		console.log('Connected to wss!');
-	// 	});
-	// 	socket.addEventListener('message', messageHandler);
-	// 	socket.addEventListener('close', event => {
-	// 		tries < 8 && tries++;
-	// 		var delay = Math.floor((Math.random() + 0.5) * Math.pow(1.5, tries));
-	// 		console.log('Disconnected from socket. Reconnecting with random exponential backoff (' + delay + ' seconds)...');
-	// 		setTimeout(this.connect.bind(this, uri, messageHandler, tries), 1000 * delay);
-	// 	});
-	// 	return socket;
-	// }
 
 	update(data) {
 		this.setState({ data: data });
@@ -73,13 +53,6 @@ class Leaderboard3 extends React.Component {
 	render() {
         return <Leaderboard title='Dorm Leaderboard' data={this.state.data} />
 	}
-
-	// componentDidUpdate() {
-	// 	var loader = document.getElementsByClassName('loader')[0];
-	// 	loader.classList.remove('initial');
-	// 	loader.style.opacity = 0;
-	// 	loader.style.visibility = 'hidden';
-	// }
 
 }
 
