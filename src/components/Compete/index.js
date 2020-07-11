5aad89662727d14cfc9a450faa5004482585fad3
Amy Qian <aqian@g.hmc.edu>
2020-07-11 11:31:24 -0700

    added leaderboard table material ui

--- src/components/Compete/index.js
+++ src/components/Compete/index.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { withFirebase } from '../Firebase';
 import { AuthUserContext, withAuthorization } from "../Session";
 
+import Leaderboard from "../Leaderboard"
+
 const CompetePage = () => (
     <div>
     <AuthUserContext>
@@ -10,6 +12,10 @@ const CompetePage = () => (
       <div class="base-container">
         <h1>Competition! Yay!<span role="img" aria-label="Trophy">🏆</span></h1>
         <center>New competitions coming soon...</center>
+        <h3>Leaderboard</h3>
+        <p>See your dorm's ranking below! :)</p>
+        <h1></h1>
+        <Leaderboard />
       </div>
     )}
     </AuthUserContext>
