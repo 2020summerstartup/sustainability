6d425d90edc3f31b7330c9d2d7057f29bde560e2
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-15 13:58:03 -0400

    Cleaning up code

--- src/components/ActionCard/index.js
+++ src/components/ActionCard/index.js
@@ -24,7 +24,6 @@ import ActionData from "../ActionData/index.json";
 import { updateUserPoint, updateDormPoint, updateUserDorm } from "../Firebase";
 import { AuthUserContext, withAuthorization} from "../Session";
 
-// I pulled these from Home's index.js
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -122,14 +121,6 @@ const ActionCard = () => {
   };
 
   // KEEP THIS!!! UPDATED VERSION
-  //   const increment = () => {
-  //     // add specified number of points to the saved point total
-  //     localStorage.setItem(currSusAction, parseInt(localStorage.getItem(currSusAction))+parseInt(`${points}`));
-  //     updateUser(authContext.email, susAction, points).then(() => {window.location.reload(true)})
-  //   };
-  // };
-
-  // KEEP THIS!!! UPDATED VERSION
   const increment = (action) => {
     // add specified number of points to the saved point total
     localStorage.setItem(
@@ -168,20 +159,17 @@ const ActionCard = () => {
 
   };
 
-  // OKAY HERE'S THE PLAN. I RAN A FOR LOOP HERE FOR EVERY THING IN ACTION DATA (LIKE I DID ELSEWHERE FOR INITALIZE STUFF)
-  // I SET A VARIABLE FOR EACH COLOR TO BE ONE OF TWO THINGS
-  // THEN WITHIN THE STYLE I SET THE COLOR TO BE EQUAL TO THE VARIABLE THAT WAS SET HERE.
-  //var favColor = "blue";
-  var favIconColors = []
-  for(const key in ActionData) {
-    var action = ActionData[key];
-    //favIconColors[key-1] = "green";
+  // Initialize the color of each favorite button
+  // This isn't in a function because I can't call the function when I want using html. Could go in a function and then be called with JS.
+  var favIconColors = [] // Initalize array of the color for each favIcon
+  for(const key in ActionData) { // Iterate over every action in ActionData
+    var action = ActionData[key]; // Take the current action
     var storageName = action.susAction.concat("Fav");
-    var storedFav = localStorage.getItem(storageName) == 'true'; // We're getting a warning in the console
-    if (storedFav) {
-    favIconColors[key-1] = "#DC143C"; // red
+    var storedFav = localStorage.getItem(storageName) == 'true'; // We're getting a warning in the console (wants ===)
+    if (storedFav) { // If the action is favorited
+    favIconColors[key-1] = "#DC143C"; // Turn red
     } else {
-      favIconColors[key-1] = "#6c6c6c"; // gray
+      favIconColors[key-1] = "#6c6c6c"; // Otherwise turn gray
     }
   }
 
@@ -213,30 +201,6 @@ const ActionCard = () => {
       toast.warn(message, { autoClose: 5000 });
     }
     localStorage.setItem(storageName, storedFav); // Save the updated favorite value
-  };
-
-  const initColor = (action) => {
-    // Get the name and info of the stored action that we're working with
-    var storageName = action.susAction.concat("Fav");
-    // storedFav is a boolean (is the current action favorited?)
-    // NOTE: the item in storage is a string, so the following line forces it to evaluate as a boolean
-    var storedFav = localStorage.getItem(storageName) == 'true';
-    // In case the action hasn't been favorited before
-    // NOTE: false is NaN, so here I don't check if the boolean is NaN because it often is.
-    if (storedFav == null) {
-      console.log("storedFav was null or NaN", storedFav);
-      storedFav = false; // If not initialized, initialize here
-    }
-    // variable for getting color of fav icon
-    var favIconColor = document.getElementById("favoriteIcon".concat(action.susAction));
-    // favIconColor was not null when this was called in onclick so the function itself works, it's the calling that's messed up
-    console.log('favIconColor', favIconColor);
-    // Notify user that action was added/removed from favorites
-    if (storedFav) {
-      favIconColor.style.color = "#DC143C"; // Turn red
-    } else {
-      favIconColor.style.color = "#6c6c6c"; // Back to grey
-    }
   };
 
   return (
@@ -286,7 +250,7 @@ const ActionCard = () => {
                     <IconButton
                       aria-label="add to favorites"
                       style={{ backgroundColor: "transparent" }}
-                      style={{ color: favIconColors[i-1] }} // TODO: Might have an off by one error here!!
+                      style={{ color: favIconColors[i-1] }} // Set the favIcon color (i-1 prevents off-by-one error)
                       onClick={() => favAction(action)}
                       id={ "favoriteIcon".concat(action.susAction) }
                       className={classes.favoriteIcon}
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -52,57 +52,6 @@ function assignData(data){
     localStorage.setItem(key, value)
   }
 }
-// Loop over every element in ActionData, adding the save point values earn from each
-// for(const key in ActionData) {
-//   total += parseInt(localStorage.getItem(ActionData[key].susAction));
-// }
-// Save the total point value in local storage (to be accessed elsewhere when we need to display total)
-// localStorage.setItem('total', total);
-
-// Initialize total points variable
-// TODO: I want this to update without us having to manually add every sus action. Change to a function somehow
-// var total = parseInt(localStorage.getItem('waterBottle')) + parseInt(localStorage.getItem('cmontWalk'))
-// + parseInt(localStorage.getItem('reuseStraw')) + parseInt(localStorage.getItem('reuseBag'))
-// + parseInt(localStorage.getItem('frmersMarket')) + parseInt(localStorage.getItem('rebrewTea'))
-// + parseInt(localStorage.getItem('noFoodWaste')) + parseInt(localStorage.getItem('meatlessMon'))
-// + parseInt(localStorage.getItem('ecoClean'));
-
-// // Initialize total points variable
-// // TODO: I want this to update without us having to manually add every sus action.
-// var total = parseInt(localStorage.getItem('waterBottle')) + parseInt(localStorage.getItem('cmontWalk'))
-// + parseInt(localStorage.getItem('reuseStraw')) + parseInt(localStorage.getItem('reuseBag'))
-// + parseInt(localStorage.getItem('frmersMarket')) + parseInt(localStorage.getItem('rebrewTea'))
-// + parseInt(localStorage.getItem('noFoodWaste')) + parseInt(localStorage.getItem('meatlessMon'))
-// + parseInt(localStorage.getItem('ecoClean'));
-
-
-// The following commented out code didn't work, but I want to keep the record of it for now
-// to understand what I tried and what went wrong. Talk to me (Katie) if you want any clarificaiton. :)
-// // If the counter changes (i.e. if the buzz or undo button is pressed), call refreshPage function
-// Counter.onChange = refreshPage();
-
-// // Refresh the page
-// function refreshPage() {
-//   console.log('refreshPage func ran');
-//   console.log('total val', total);
-//   console.log('sum value', parseInt(localStorage.getItem('waterBottle')) + parseInt(localStorage.getItem('cmontWalk')));
-//   if (total != parseInt(localStorage.getItem('waterBottle')) + parseInt(localStorage.getItem('cmontWalk'))) {
-//     console.log('if ran');
-//     total = parseInt(localStorage.getItem('waterBottle')) + parseInt(localStorage.getItem('cmontWalk'));
-//     document.location.reload(true);
-//   } else {
-//     console.log('else ran');
-//   }
-//   // Currently the above if statement never runs, so the computer thinks that testParam is always equal to 
-//   // the actual local storage values. but the local storage should be getting changed, and testParam definitely
-//   // isn't updated.
-//   // And the function is basically running full time so that shouldn't be the issue
-
-//   // update at 2:43: I think the issue is that the function isn't actually running. Which I guess kind of makes
-//   // sense because this entire file runs once and then it stops until reload. So i probably have to move all of this
-//   // code into counter or something.
-// }
-
 
 // need this for modal to not get error in console
 Modal.setAppElement("#root");
