04ac6dc955370e69c058a243623cb10516832d58
Amy Qian <aqian@g.hmc.edu>
2020-07-16 00:31:12 -0700

    fixed homepage tabs!!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Modal from "react-modal";
 import Confetti from "react-confetti";
 import { AuthUserContext, withAuthorization } from "../Session";
-import {getUser, createUser, uploadUserTotalPoint} from "../Firebase";
+import { getUser, createUser, uploadUserTotalPoint } from "../Firebase";
 
 const CustomToast = ({ closeToast }) => {
   return (
@@ -75,7 +75,6 @@ const modalCustomStyles = {
 // need this for modal to not get error in console
 Modal.setAppElement("#root");
 
-
 // Text to display on the homepage
 function HomePage() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -83,8 +82,8 @@ function HomePage() {
   const authContext = useContext(AuthUserContext);
 
   // get user's dorm set in local storage
-  getUser(authContext.email).onSnapshot(docSnapshot => {
-
+  getUser(authContext.email).onSnapshot(
+    (docSnapshot) => {
       if (docSnapshot.exists) {
         assignData(docSnapshot.data());
         function assignData(data) {
@@ -95,96 +94,114 @@ function HomePage() {
         initPoints(authContext.email);
         uploadUserTotalPoint(authContext.email, total);
       }
-    }, err => {
-    console.log(`Encountered error: ${err}`); 
-  });
+    },
+    (err) => {
+      console.log(`Encountered error: ${err}`);
+    }
+  );
 
   const clicked = () => {
     // var codeBlock = '<button id=\'wrapper\' >Another option...</button><br />';
-    var codeBlock = '';
+    var codeBlock = "";
     for (const key in ActionData) {
-      codeBlock += ActionData[key].title.concat(' Points: ', localStorage.getItem(ActionData[key].susAction), ' ') +
-      '<br />';
+      codeBlock +=
+        ActionData[key].title.concat(
+          " Points: ",
+          localStorage.getItem(ActionData[key].susAction),
+          " "
+        ) + "<br />";
     }
 
     // Inserting the code block to wrapper element
-    document.getElementById("wrapper").innerHTML = codeBlock
-  }
+    document.getElementById("wrapper").innerHTML = codeBlock;
+  };
 
   var message = [];
   total = localStorage.getItem("total");
 
-
   return (
-    <div className="base-container">
-       < HomeTabs />
-      <h1>Home Sweet Home</h1>
-      {/* Testing for fun */}
-      <h3>
-        You have earned&nbsp;
-        {<CountUp start={0} end={total} duration={1}></CountUp>} points! &nbsp;
-        {/* <button onClick={notify2} className="button">
+    <>
+      <HomeTabs />
+      <div className="base-container">
+        <h1>Home Sweet Home</h1>
+        {/* Testing for fun */}
+        <h3>
+          You have earned&nbsp;
+          {<CountUp start={0} end={total} duration={1}></CountUp>} points!
+          &nbsp;
+          {/* <button onClick={notify2} className="button">
           Click me!
         </button> */}
-      </h3>
-      <button onClick={() => setModalIsOpen(true)} className="button">
-        Check Your Progress
-      </button>
-      <p></p>
-      <CustomizedDialogs />
-      <p></p>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        className={styles.modal}
-        overlayClassName={styles.overlay}
-        // style={{
-        //   overlay: {
-        //     // backgroundColor: 'papayawhip'
-        //   },
-        //   content: {
-        //     color: "var(--theme)",
-        //     overflow: "hidden",
-        //     width: "20rem",
-        //     height: "30rem",
-        //     margin: "auto auto",
-        //     verticalAlign: "middle",
-        //     textAlign: "center",
-        //   },
-        // }}
-        // style={modalCustomStyles}
-      >
-      <center>
-        {/* <Confetti
+        </h3>
+        <button onClick={() => setModalIsOpen(true)} className="button">
+          Check Your Progress
+        </button>
+        <p></p>
+        {/* <CustomizedDialogs /> */}
+        <p></p>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+          className={styles.modal}
+          overlayClassName={styles.overlay}
+          // style={{
+          //   overlay: {
+          //     // backgroundColor: 'papayawhip'
+          //   },
+          //   content: {
+          //     color: "var(--theme)",
+          //     overflow: "hidden",
+          //     width: "20rem",
+          //     height: "30rem",
+          //     margin: "auto auto",
+          //     verticalAlign: "middle",
+          //     textAlign: "center",
+          //   },
+          // }}
+          // style={modalCustomStyles}
+        >
+          <center>
+            {/* <Confetti
           width={1500}
           numberOfPieces={2000}
           recycle={false}
           opacity={0.7}
           // colors={["grey", "white", "green", "black"]}
-        /> */} 
-        {/* Just for while I'm working */}
-        {
-          // I don't yet understand what "Object" is referring to here/how the program knows that.
-          Object.keys(ActionData).map(
-            (key) => {
-              message[parseInt(key) - 1] = ActionData[key].title.concat(' Points: ', localStorage.getItem(ActionData[key].susAction), ' ')
-              return '' // It has to return a value. I think it isn't bad practice to do this? -Katie
+        /> */}
+            {/* Just for while I'm working */}
+            {
+              // I don't yet understand what "Object" is referring to here/how the program knows that.
+              Object.keys(ActionData).map((key) => {
+                message[parseInt(key) - 1] = ActionData[key].title.concat(
+                  " Points: ",
+                  localStorage.getItem(ActionData[key].susAction),
+                  " "
+                );
+                return ""; // It has to return a value. I think it isn't bad practice to do this? -Katie
+              })
             }
-          )
-        }
-        <h1>Your Progress:</h1>
-        {/* TODO: This is a super janky but slightly prettier way to display the individual points. Still need to improve later. */}
-        <p> { message[0] } <br /> { message[1] } <br /> { message[2] } <br /> { message[3] } <br /> { message[4] } <br /> { message[5] } <br /> { message[6] } <br /> { message[7] } <br /> { message[8] } { message.slice(9, message.length) } </p>
-        <h3>Total Points: { total } </h3>
-        <h1 id='wrapper'><button onClick={() => clicked()}>Another option...</button></h1>
-        <div>
-          <button onClick={() => setModalIsOpen(false)} className="button">
-            Close
-          </button>
-        </div>
-      </center>
-      </Modal>
-    </div>
+            <h1>Your Progress:</h1>
+            {/* TODO: This is a super janky but slightly prettier way to display the individual points. Still need to improve later. */}
+            <p>
+              {" "}
+              {message[0]} <br /> {message[1]} <br /> {message[2]} <br />{" "}
+              {message[3]} <br /> {message[4]} <br /> {message[5]} <br />{" "}
+              {message[6]} <br /> {message[7]} <br /> {message[8]}{" "}
+              {message.slice(9, message.length)}{" "}
+            </p>
+            <h3>Total Points: {total} </h3>
+            <h1 id="wrapper">
+              <button onClick={() => clicked()}>Another option...</button>
+            </h1>
+            <div>
+              <button onClick={() => setModalIsOpen(false)} className="button">
+                Close
+              </button>
+            </div>
+          </center>
+        </Modal>
+      </div>
+    </>
   );
 }
 
