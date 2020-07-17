29f9194ac2dc7f231347ad958bedded9884573d9
Amy Qian <aqian@g.hmc.edu>
2020-07-17 11:54:00 -0700

    styled modalIncrement for mobile screens

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -59,23 +59,6 @@ function assignData(data) {
   }
 }
 
-const modalCustomStyles = {
-  overlay: {
-    position: "fixed",
-    overflow: "hidden",
-  },
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    overflow: "hidden",
-    height: "25rem",
-  },
-};
-
 // need this for modal to not get error in console
 Modal.setAppElement("#root");
 
