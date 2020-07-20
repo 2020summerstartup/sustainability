b210a2c6148653addf2e2eca6ddb79641847b803
Amy Qian <aqian@g.hmc.edu>
2020-07-19 23:39:51 -0700

    delete unused imports

--- src/pages/InfoPage/muiContactForm.js
+++ src/pages/InfoPage/muiContactForm.js
@@ -4,17 +4,9 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 // for class Components we should use withStyles instead of makeStyles
 import { withStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import InputAdornment from "@material-ui/core/InputAdornment";
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import PersonIcon from '@material-ui/icons/Person';
 import EmailIcon from '@material-ui/icons/Email';
