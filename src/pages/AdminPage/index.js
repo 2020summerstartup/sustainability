1396e0ad72076874d46d87abefd067c7f92ebc15
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-23 19:14:23 -0400

    adding admin page/cleanup/challenges

--- src/pages/AdminPage/index.js
+++ src/pages/AdminPage/index.js
@@ -1,21 +1,25 @@
 import React from "react"; // No longer imports component because it wasn't used
 import { AuthUserContext, withAuthorization } from "../../services/Session";
-
 import Paper from "@material-ui/core/Paper";
+import ChallengeData from '../CompetePage/challengeData.json';
 
-const AdminPage = () => (
-  <div>
-    <AuthUserContext>
-      {(authUser) => (
-        <Paper>
-          <div className="base-container">
-              <center>THIS IS THE ADMIN PAGE.</center>
-          </div>
-        </Paper>
-      )}
-    </AuthUserContext>
-  </div>
-);
+function AdminPage() {
+
+  // Text to be displayed on the admin page
+  return (
+    <div>
+      <AuthUserContext>
+        {(authUser) => (
+          <Paper>
+            <div className="base-container">
+                <center>THIS IS THE ADMIN PAGE.</center>
+            </div>
+          </Paper>
+        )}
+      </AuthUserContext>
+    </div>
+  ); // End of return statement
+}
 
 const condition = (authUser) => !!authUser;
 
