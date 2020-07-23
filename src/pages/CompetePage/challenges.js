a15bbd17ebb79ae2aaf622cfc6dd74b4673f839c
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-23 14:06:36 -0400

    created skeleton of challenges

---
+++ src/pages/CompetePage/challenges.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function challenges() {
+    return (
+        <div>
+            <h1>
+                Current Challenges
+            </h1>
+            display current challenges from a json file? Idk what we want to do with this, but the skeleton of it is here now!
+        </div>
+    )
+}
