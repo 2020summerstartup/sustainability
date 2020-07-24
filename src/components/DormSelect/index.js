cf972361d91ae8c10c37ecd1729cec0e4cfa1b95
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-24 19:27:36 -0400

    Deleted unused code

--- src/components/DormSelect/index.js
+++ src/components/DormSelect/index.js
@@ -1,10 +1,8 @@
 import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import NativeSelect from "@material-ui/core/NativeSelect";
 
 import { AuthUserContext } from "../../services/Session";
 import { updateUserDorm, getDorm } from "../../services/Firebase";
