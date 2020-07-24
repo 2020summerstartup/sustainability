de89f1bd3882c1b1401225dd4b04a41d31a082f0
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-24 12:59:34 -0400

    Playing with admin adding challenges

--- src/pages/AdminPage/index.js
+++ src/pages/AdminPage/index.js
@@ -3,19 +3,42 @@ import { AuthUserContext, withAuthorization } from "../../services/Session";
 
 import Paper from "@material-ui/core/Paper";
 
-const AdminPage = () => (
+function AdminPage() {
+
+  var challengeData2=[
+    {
+        "title": "Recycling",
+        "info": "further description of the challenge here. (Maybe double points for recycling or something)"
+    },
+    {
+        "title": "Decrease Energy Use",
+        "info": "here's some more info about the second thing"
+    }
+]
+
+const getInfo = () => {
+  var retVal = prompt('Choose a title for your new challenge');
+  console.log("ChallengeData2 before change", challengeData2);
+  console.log('you entered', retVal);
+  challengeData2[challengeData2.length] = {"title": retVal};
+  console.log("Changed ChallengeData2", challengeData2);
+}
+
+  return (
   <div>
     <AuthUserContext>
       {(authUser) => (
         <Paper>
           <div className="base-container">
               <center>THIS IS THE ADMIN PAGE.</center>
+              <button onClick={() => getInfo()}>Add a new challenge</button>
           </div>
         </Paper>
       )}
     </AuthUserContext>
   </div>
-);
+  )
+}
 
 const condition = (authUser) => !!authUser;
 
