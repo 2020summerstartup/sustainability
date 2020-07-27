aca84cc27afc0f435419fb5da60318ce9f74adac
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-27 16:09:08 -0400

    Deleted unused imports

--- src/pages/HomePage/badgeModal.js
+++ src/pages/HomePage/badgeModal.js
@@ -5,10 +5,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
 
 import badgeImg from "../../img/badge.svg";
 import badge from "../../sounds/hero_simple-celebration-01.wav";
