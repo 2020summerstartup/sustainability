c77ed42393140acb44984d70a721cfdd3746d391
Amy Qian <aqian@g.hmc.edu>
2020-07-28 13:52:55 -0700

    react lazy router

--- src/App/index.js
+++ src/App/index.js
@@ -4,13 +4,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navigation from "../components/Navigation";
 import BottomNav from "../components/Navigation/bottomNav";
 import MuiLandingPage from "../pages/LandingPage/muiLandingPage";
-import MuiSignInPage from "../pages/RegisterPage/muiSignInPage";
-import MuiSignUpPage from "../pages/RegisterPage/muiSignUpPage";
-// import SignUpPage from "../pages/RegisterPage/signUpPage";
-import MuiPasswordForgetPage from "../pages/RegisterPage/muiPasswordForgetPage";
-import HomePage from "../pages/HomePage";
-import AccountPage from "../pages/AccountPage";
-import InfoPage from "../pages/InfoPage";
+// import MuiSignInPage from "../pages/RegisterPage/muiSignInPage";
+// import MuiSignUpPage from "../pages/RegisterPage/muiSignUpPage";
+// import MuiPasswordForgetPage from "../pages/RegisterPage/muiPasswordForgetPage";
+// import HomePage from "../pages/HomePage";
+// import AccountPage from "../pages/AccountPage";
+// import InfoPage from "../pages/InfoPage";
+// import CompetePage from "../pages/CompetePage";
+// import MuiOfflinePage from "../pages/OfflinePage/muiOfflinePage";
+// import MuiChangePw from "../pages/AccountPage/Settings/muiChangePw";
+// import MuiChangeDorm from "../pages/AccountPage/Settings/muiChangeDorm";
+// import DeleteAccount from "../pages/AccountPage/Settings/deleteAccount";
+// import AdminPage from "../pages/AdminPage";
 import Header, {
   HomeHeader,
   CompeteHeader,
@@ -18,41 +23,117 @@ import Header, {
   AccountHeader,
   BackArrowHeader,
 } from "../components/Headers";
-import CompetePage from "../pages/CompetePage";
-import MuiOfflinePage from "../pages/OfflinePage/muiOfflinePage";
-import MuiChangePw from "../pages/AccountPage/Settings/muiChangePw";
-import MuiChangeDorm from "../pages/AccountPage/Settings/muiChangeDorm";
-import DeleteAccount from "../pages/AccountPage/Settings/deleteAccount";
-import AdminPage from "../pages/AdminPage";
 import * as ROUTES from "../constants/routes";
 
 import { withAuthentication } from "../services/Session";
 import { withTheme } from "../components/Theme";
 
-const AppBase = () => (
-  <Router>
-    <Switch>
-      {/* FOR PAGES WITH SPECIAL HEADERS */}
-      <Route exact path="/home" component={HomeHeader} />
-      <Route exact path="/compete" component={CompeteHeader} />
-      <Route exact path="/info" component={InfoHeader} />
-      <Route exact path="/account" component={AccountHeader} />
-      <Route exact path="/deleteaccount" component={BackArrowHeader} />
-      <Route exact path="/signup" component={BackArrowHeader} />
-      <Route exact path="/changedorm" component={BackArrowHeader} />
-      <Route exact path="/changepassword" component={BackArrowHeader} />
-      <Route exact path="/forgetpassword" component={BackArrowHeader} />
-      <Route component={Header} />
-    </Switch>
 
-    <div className="main">
-      {/* {window.location.pathname !== "/account" ? <Header /> : <AccountHeader /> }  */}
+// React lazy
+// const MuiLandingPage = lazy(( ) => import("../pages/LandingPage/muiLandingPage"));
+const MuiSignInPage = lazy(() => import("../pages/RegisterPage/muiSignInPage"));
+const MuiSignUpPage = lazy(() => import("../pages/RegisterPage/muiSignUpPage"));
+const MuiPasswordForgetPage = lazy(() =>
+  import("../pages/RegisterPage/muiPasswordForgetPage")
+);
+const HomePage = lazy(() => import("../pages/HomePage"));
+const AccountPage = lazy(() => import("../pages/AccountPage"));
+const InfoPage = lazy(() => import("../pages/InfoPage"));
+const CompetePage = lazy(() => import("../pages/CompetePage"));
+const MuiOfflinePage = lazy(() =>
+  import("../pages/OfflinePage/muiOfflinePage")
+);
+const MuiChangePw = lazy(() =>
+  import("../pages/AccountPage/Settings/muiChangePw")
+);
+const MuiChangeDorm = lazy(() =>
+  import("../pages/AccountPage/Settings/muiChangeDorm")
+);
+const DeleteAccount = lazy(() =>
+  import("../pages/AccountPage/Settings/deleteAccount")
+);
+const AdminPage = lazy(() => import("../pages/AdminPage"));
 
-      <Navigation />
-      <BottomNav />
-
+function AppBase() {
+  return (
+    <Router>
       <Switch>
-        {/* For each page's content */}
+        {/* FOR PAGES WITH SPECIAL HEADERS */}
+        <Route exact path="/home" component={HomeHeader} />
+        <Route exact path="/compete" component={CompeteHeader} />
+        <Route exact path="/info" component={InfoHeader} />
+        <Route exact path="/account" component={AccountHeader} />
+        <Route exact path="/deleteaccount" component={BackArrowHeader} />
+        <Route exact path="/signup" component={BackArrowHeader} />
+        <Route exact path="/changedorm" component={BackArrowHeader} />
+        <Route exact path="/changepassword" component={BackArrowHeader} />
+        <Route exact path="/forgetpassword" component={BackArrowHeader} />
+        <Route component={Header} />
+      </Switch>
+
+      <div className="main">
+        {/* {window.location.pathname !== "/account" ? <Header /> : <AccountHeader /> }  */}
+
+        <Navigation />
+        <BottomNav />
+
+        <Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            {/* For each page's content */}
+            <Route
+              exact
+              path={ROUTES.LANDING}
+              component={(props) => <MuiLandingPage {...props} />}
+            />
+            <Route
+              path={ROUTES.SIGN_UP}
+              component={(props) => <MuiSignUpPage {...props} />}
+            />
+            <Route
+              path={ROUTES.SIGN_IN}
+              component={(props) => <MuiSignInPage {...props} />}
+            />
+            <Route
+              path={ROUTES.PASSWORD_FORGET}
+              component={(props) => <MuiPasswordForgetPage {...props} />}
+            />
+            <Route
+              path={ROUTES.HOME}
+              component={(props) => <HomePage {...props} />}
+            />
+            <Route
+              path={ROUTES.ACCOUNT}
+              component={(props) => <AccountPage {...props} />}
+            />
+            <Route path={ROUTES.INFO} render={() => <InfoPage />} />
+            <Route
+              path={ROUTES.COMPETE}
+              component={(props) => <CompetePage {...props} />}
+            />
+            <Route
+              path={ROUTES.OFFLINE}
+              component={(props) => <MuiOfflinePage {...props} />}
+            />
+            <Route
+              path={ROUTES.CHANGEPW}
+              component={(props) => <MuiChangePw {...props} />}
+            />
+            <Route
+              path={ROUTES.CHANGEDORM}
+              component={(props) => <MuiChangeDorm {...props} />}
+            />
+            <Route
+              path={ROUTES.DELETE_ACCOUNT}
+              component={(props) => <DeleteAccount {...props} />}
+            />
+            <Route
+              path={ROUTES.ADMIN}
+              component={(props) => <AdminPage {...props} />}
+            />
+          </Suspense>
+        </Switch>
+
+        {/* <Switch>
         <Route exact path={ROUTES.LANDING} component={MuiLandingPage} />
         <Route path={ROUTES.SIGN_UP} component={MuiSignUpPage} />
         <Route path={ROUTES.SIGN_IN} component={MuiSignInPage} />
@@ -69,10 +150,19 @@ const AppBase = () => (
         <Route path={ROUTES.CHANGEDORM} component={MuiChangeDorm} />
         <Route path={ROUTES.DELETE_ACCOUNT} component={DeleteAccount} />
         <Route path={ROUTES.ADMIN} component={AdminPage} />
-      </Switch>
-    </div>
-  </Router>
-);
+      </Switch> */}
+      </div>
+    </Router>
+  );
+}
+
+// function WaitingComponent(Component) {
+//   return (props) => (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <Component {...props} />
+//     </Suspense>
+//   );
+// }
 
 const App = withTheme(AppBase);
 export default withAuthentication(App);
