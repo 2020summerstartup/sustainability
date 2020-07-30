ad128a50c70d09e1215d4d48db9719c035c11c53
Amy Qian <aqian@g.hmc.edu>
2020-07-30 00:06:52 -0700

    color animation border & add to home screen ribbon

--- src/pages/InfoPage/index.js
+++ src/pages/InfoPage/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import InfoCards from "./infoCards";
+import AddtoHomeScreen from "./addToHomeScreen";
 import FBContactForm from "./fbContactForm";
 
 const InfoPage = () => (
   <div className="base-container">
     <InfoCards />
+    <AddtoHomeScreen />
     <FBContactForm />
   </div>
 );
