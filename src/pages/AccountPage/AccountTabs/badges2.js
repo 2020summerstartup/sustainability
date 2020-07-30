8f59953f8eb71bd41ef1d7d40eb63b7aecb275e7
Amy Qian <aqian@g.hmc.edu>
2020-07-30 15:11:39 -0700

    mapping through badges

--- src/pages/AccountPage/AccountTabs/badges2.js
+++ src/pages/AccountPage/AccountTabs/badges2.js
@@ -1,104 +1,188 @@
 import React from "react";
 import styles from "./badges2.module.css";
 
-import { makeStyles } from "@material-ui/core/styles";
-import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
-import EcoIcon from "@material-ui/icons/Eco";
-// import Card from "@material-ui/core/Card";
-// import CardContent from "@material-ui/core/CardContent";
-// import CardMedia from "@material-ui/core/CardMedia";
-// import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  content: {
-    flex: "1 0 auto",
-  },
-  cover: {
-    // width: 151,
-    width: "100%",
-  },
-  buzzTimes: {
-    marginTop: "1rem",
-  },
-  avatar: {
-    color: theme.palette.secondary.main,
-    backgroundColor: theme.palette.primary.main,
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-    border: "3px solid white",
-    margin: "auto",
-  },
-}));
+class Badge2 extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      badges: [],
+    };
+    this.getData = this.getData.bind(this);
+  }
 
-export default function MediaControlCard() {
-  const classes = useStyles();
+  getData() {
+    let data = {
+      success: true,
+      badges: [
+        {
+          id: 1,
+          title: "Recycle Badge",
+          styling: null,
+        },
+        {
+          id: 3,
+          title: "Walking Badge",
+          styling: null,
+        },
+        {
+          id: 2,
+          title: "Straw Badge",
+          styling: null,
+        },
+        {
+          id: 4,
+          title: "Bag Badge",
+          styling: null,
+        },
+        {
+          id: 5,
+          title: "Market Badge",
+          styling: null,
+        },
+        {
+          id: 6,
+          title: "Tea Badge",
+          styling: null,
+        },
+        {
+          id: 7,
+          title: "No Waste Badge",
+          styling: null,
+        },
+        {
+          id: 8,
+          title: "Meatless Badge",
+          styling: null,
+        },
+        {
+          id: 9,
+          title: "Cleaning Badge",
+          styling: null,
+        },
+        {
+          id: 10,
+          title: "Transportation Badge",
+          styling: null,
+        },
+        {
+          id: 11,
+          title: "Clothing Badge",
+          styling: null,
+        },
+        {
+          id: 12,
+          title: "Air Dry Badge",
+          styling: null,
+        },
+        {
+          id: 13,
+          title: "Climate Badge",
+          styling: null,
+        },
+      ],
+    };
+    badges = this.assignStyling(data.badges);
+    this.setState({
+      badges: badges,
+    });
+  }
 
-  return (
-    <div className={styles.root}>
-      <div className={styles.leaves}>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
-        <i></i>
+  assignStyling(badges) {
+    badges.forEach((badge) => {
+      if (badge.id % 2 === 0) {
+        badge.styling = styles.titleLeft;
+      } else {
+        badge.styling = styles.titleRight;
+      }
+    })
+    return badges
+  };
+
+  componentWillMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <Typography variant="h5">
+          Congratulations {localStorage.getItem("name")}! You have earned
+          [number] badges!
+        </Typography>
+        <div className={styles.leaves}>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+        </div>
+
+        {this.state.badges ? (
+          this.state.badges.map((badge, i) => (
+            <div key={i}>
+              <div className={styles.column}>
+                <div className={styles.left}>
+                  <Typography variant="h6" className={styles.titleLeft}>
+                    {badge.title}
+                  </Typography>
+                </div>
+                <div className={styles.right}>
+                  <Typography variant="h6" className={styles.titleRight}>
+                    {badge.title}
+                  </Typography>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="empty">
+            <Typography variant="h5">
+              You haven't earned any badges yet :(
+            </Typography>
+          </div>
+        )}
+        {/* <div className={styles.column}>
+          <div className={styles.left}>
+            <Typography variant="h6" className={styles.titleLeft}>
+              Recyling Badge
+            </Typography>
+          </div>
+          <div className={styles.right}>
+            <Typography variant="h6" className={styles.titleRight}>
+              Cleaning Badge
+            </Typography>
+          </div>
+          <div className={styles.left}>
+            <Typography variant="h6" className={styles.titleLeft}>
+              Gardening Badge
+            </Typography>
+          </div>
+          <div className={styles.right}>
+            <Typography variant="h6" className={styles.titleRight}>
+              No Waste Badge
+            </Typography>
+          </div>
+        </div> */}
       </div>
-      <div className={styles.column}>
-        <div className={styles.left}>
-          <Typography variant="h6" className={styles.titleLeft}>
-            Recyling Badge
-          </Typography>
-        </div>
-        <div className={styles.right}>
-          <Typography variant="h6" className={styles.titleRight}>
-            Cleaning Badge
-          </Typography>
-        </div>
-        <div className={styles.left}>
-          <Typography variant="h6" className={styles.titleLeft}>
-            Gardening Badge
-          </Typography>
-        </div>
-        <div className={styles.right}>
-          <Typography variant="h6" className={styles.titleRight}>
-            No Waste Badge
-          </Typography>
-        </div>
-      </div>
-    </div>
-    // <Card className={classes.root}>
-    //   <div className={classes.details}>
-    //     <CardContent className={classes.content}>
-    //       <Typography variant="h5">
-    //         Earth Day Badge
-    //       </Typography>
-    //       <Typography variant="subtitle1" color="textSecondary" className={classes.buzzTimes}>
-    //         Buzzed 20 times!
-    //       </Typography>
-    //     </CardContent>
-    //   </div>
-    //   <CardMedia
-    //     className={classes.cover}
-    //     image="https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    //     title="Earth Day"
-    //   />
-    // </Card>
-  );
+    );
+  }
 }
+
+export default Badge2;
