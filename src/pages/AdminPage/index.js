545a75fb0f5e7e560d3fa98c5e2579303d10027d
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-31 14:01:16 -0400

    Added some functionality to add challenge

--- src/pages/AdminPage/index.js
+++ src/pages/AdminPage/index.js
@@ -3,7 +3,12 @@ import { AuthUserContext, withAuthorization } from "../../services/Session";
 
 import Paper from "@material-ui/core/Paper";
 
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function AdminPage() {
+
+  toast.configure(); // Configure to be able to display goasts to user later in the file
 
   var challengeData2=[
     {
@@ -17,11 +22,22 @@ function AdminPage() {
 ]
 
 const getInfo = () => {
-  var retVal = prompt('Choose a title for your new challenge');
-  console.log("ChallengeData2 before change", challengeData2);
-  console.log('you entered', retVal);
-  challengeData2[challengeData2.length] = {"title": retVal};
-  console.log("Changed ChallengeData2", challengeData2);
+  var enteredTitle = prompt('Choose a title for your new challenge');
+  var enteredInfo = prompt('Enter an explanation of the new challenge');
+  // var confirmationMessage = 
+  var confirmed = window.confirm('Here is your new challenge: Title: '.concat(enteredTitle, ' Info: ', enteredInfo, ' Are you sure you want to add this challenge?')); // , 'Info:', enteredInfo, 'Are you sure you want to add this challenge?'
+  if (confirmed) {
+    console.log('user confirmed');
+    toast.success('New challenge will be added! (not actually we need to get it working lol)', { autoClose: 5000 }); // It's "success" so that the window is green
+  } else {
+    console.log('user aborted');
+    toast.error('Challenge was not added', { autoClose: 5000 }); // It's "error" so that the window is red
+  }
+  // console.log("ChallengeData2 before change", challengeData2);
+  console.log('new title:', enteredTitle);
+  console.log('new info:', enteredInfo);
+  challengeData2[challengeData2.length] = {"title": enteredTitle};
+  // console.log("Changed ChallengeData2", challengeData2);
 }
 
   return (
