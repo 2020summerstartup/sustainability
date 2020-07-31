929e5e13a7be65bc8a4216724ccfc18ed6c11177
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-07-31 16:29:52 -0400

    Fixed nested <p> tags

--- src/pages/InfoPage/pwa.js
+++ src/pages/InfoPage/pwa.js
@@ -4,26 +4,27 @@ import styles from "./pwa.module.css";
 // I grabbed the background color from the monochrome spread here: https://www.colorhexa.com/24a113
 // let colors = [rgb(139,139,139)];
 
-function PwaCard(){
-
-    return (
-          <div>
-            <div className="InfoCards">
-              <div className="cards">
-                  <div className={styles.cardStyle}>
-                    <h1 className="card-name">
-                        Add our app to your home screen!
+function PwaCard() {
+  return (
+    <div>
+      <div className="InfoCards">
+        <div className="cards">
+          <div className={styles.cardStyle}>
+            <h1 className="card-name">
+              Add our app to your home screen!
                     </h1>
-                    <p className="card-description">            
-                      Go green or go home is a progressive web app, simply press 
-                      <p style={{textSize: "15px", fontWeight: "bold"}}>Add to Home Screen</p>
-                      on your mobile device in a browser to download, no need to go to the app store!                        
+            <p className="card-description">
+              Go green or go home is a progressive web app, simply press
+                      </p>
+            <p style={{ textSize: "15px", fontWeight: "bold" }}>Add to Home Screen</p>
+            <p>
+              on your mobile device in a browser to download, no need to go to the app store!
                     </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-    );
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PwaCard;
