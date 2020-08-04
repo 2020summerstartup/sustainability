b70ef2b64490f58f6d88473b9c28b457ceb0089c
Amy Qian <aqian@g.hmc.edu>
2020-08-04 12:00:37 -0700

    changing color of info cards

--- src/pages/InfoPage/addToHomeScreen.js
+++ src/pages/InfoPage/addToHomeScreen.js
@@ -5,32 +5,32 @@ import styles from "./addToHomeScreen.module.css";
 import spectrum3 from "../../img/spectrum3.svg";
 // import spectrum4 from "../../img/spectrum4.svg";
 
-
 class AddToHomeScreen extends React.Component {
 
   render() {
     return (
       <div
-        style={{
-          backgroundImage: `url(${spectrum3})`,
-          backgroundSize: "cover",
-          // backgroundColor: "orange",
-          color: "white",
-          padding: "1.5rem",
-          borderRadius: "10px",
-          margin: "0 0.5rem",
-          maxWidth: "600px",
-          marginTop: "2rem",
-          position: "relative",
-        }}
-        // className={styles.cardWrapper}
+        // style={{
+        //   // backgroundImage: `url(${spectrum3})`,
+        //   backgroundSize: "cover",
+        //   background: "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
+        //   animation: "gradient 15s ease infinite",
+        //   color: "white",
+        //   padding: "1.5rem",
+        //   borderRadius: "10px",
+        //   margin: "0 0.5rem",
+        //   maxWidth: "600px",
+        //   marginTop: "2rem",
+        //   position: "relative",
+        // }}
+        className={styles.cardWrapper}
       >
         <div className={styles.ribbon}>
           <span>Special!</span>
         </div>
         <h1 className="card-name">Add App to <br/> Your Home Screen!</h1>
         <p className="card-description">
-          For IOS devices: Press the "Share Icon" at the bottom of your Safair browswer, then press the "Add to Homescreen" button.
+          For IOS devices: Press the "Share Icon" at the bottom of your Safari browser, then press the "Add to Homescreen" button.
         </p>
         <p className="card-description">
           For Andriod devices: [idk I have an iPhone sorry].
