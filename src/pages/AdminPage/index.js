86b28d968df0bccb92fd00e8d414e556aca8ecaa
Katie Johnson <katiejohnson@Katies-MacBook-Pro-4.local>
2020-08-04 14:14:48 -0400

    Cleaned up admin page

--- src/pages/AdminPage/index.js
+++ src/pages/AdminPage/index.js
@@ -12,9 +12,9 @@ function AdminPage() {
         {(authUser) => (
           <Paper>
             <div className="base-container">
-              <center>THIS IS THE ADMIN PAGE.</center>
               {/* The google form for admins to fill out to add challenges */}
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfHIX4V2KaPP8bgbmpl79E93rBVmr6Q6KH5Yu4nR7bpAiXqAg/viewform?embedded=true" width="1000rem" height="800rem" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfHIX4V2KaPP8bgbmpl79E93rBVmr6Q6KH5Yu4nR7bpAiXqAg/viewform?embedded=true" width="1000rem" height="800rem" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+              Want to remove a challenge? Email the developers at suscompetitionteam@gmail.com (or by completing the "contact us" form on the info page), and we'll get to it as soon as we can!
             </div>
           </Paper>
         )}
