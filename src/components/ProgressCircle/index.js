6c22483320513898abcb987657b00a7feb749e2b
Amy Qian <aqian@g.hmc.edu>
2020-08-04 16:37:12 -0700

    added marginTop to ProgressCircle

--- src/components/ProgressCircle/index.js
+++ src/components/ProgressCircle/index.js
@@ -3,11 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme) => ({
-  // root: {
-  //   display: 'flex',
-  //   '& > * + *': {
-  //     marginLeft: theme.spacing(2),
-  // },
+  root: {
+    marginTop: "2rem",
+    // display:'flex',
+    // '& > * + *': {
+    //   marginLeft: theme.spacing(2),
+  },
 }));
 
 export default function CircularIndeterminate() {
