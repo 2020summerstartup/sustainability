6b0a5cce4749c752b67e77ff6a7b791034f7bf39
Katie Johnson <katiejohnson@Katies-MacBook-Pro-4.local>
2020-08-04 14:35:17 -0400

    Added title to form to fix warning

--- src/pages/AdminPage/index.js
+++ src/pages/AdminPage/index.js
@@ -13,7 +13,7 @@ function AdminPage() {
           <Paper>
             <div className="base-container">
               {/* The google form for admins to fill out to add challenges */}
-              <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfHIX4V2KaPP8bgbmpl79E93rBVmr6Q6KH5Yu4nR7bpAiXqAg/viewform?embedded=true" width="1000rem" height="800rem" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+              <iframe title="challengeForm" src="https://docs.google.com/forms/d/e/1FAIpQLSfHIX4V2KaPP8bgbmpl79E93rBVmr6Q6KH5Yu4nR7bpAiXqAg/viewform?embedded=true" width="1000rem" height="800rem" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
               Want to remove a challenge? Email the developers at suscompetitionteam@gmail.com (or by completing the "contact us" form on the info page), and we'll get to it as soon as we can!
             </div>
           </Paper>
