ab3c39cb5f107d82064d127b6873e14427fe6d5d
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-08-10 14:23:08 -0400

    (FIX) Commented out html lines that nested tags

--- src/pages/CompetePage/addToHomeScreenModal.js
+++ src/pages/CompetePage/addToHomeScreenModal.js
@@ -40,7 +40,7 @@ const useStyles = makeStyles((theme) => ({
     textBody: {
         color: "white",
         margin: "0.5rem",
-        [theme.breakpoints.up('sm')]:{
+        [theme.breakpoints.up('sm')]: {
             fontSize: "16px"
         }
     },
@@ -72,14 +72,14 @@ export default function DarkModeModal() {
     useEffect(() => {
         let addHomePop_done = JSON.parse(localStorage.getItem('addHomePop_done'));
         let email = localStorage.getItem('email');
-        if (!addHomePop_done) {
-            // Displays modal and plays sound when it opens
-            setVisible(true);
-            playSound(badgeAudio);
-            // Tells firebase the action has been done
-            AddHomeOpened(email);
-        }
-        
+        // if (!addHomePop_done) {
+        // Displays modal and plays sound when it opens
+        setVisible(true);
+        playSound(badgeAudio);
+        // Tells firebase the action has been done
+        AddHomeOpened(email);
+        // }
+
     }, [badgeAudio])
     // Otherwise, does nothing
     if (!visible) return null;
@@ -94,11 +94,11 @@ export default function DarkModeModal() {
             >
                 {/* NOTE: dialogContent is styles in module.css, background wouldn't work otherwise */}
                 <DialogContent className={styles.dialogContentAddHome}>
-                    <DialogContentText id="alert-dialog-description">
-                                    <Typography variant="h5" className={classes.textTitle}>Hey {localStorage.getItem('name')}! Make sure you add our app to your homescreen!</Typography>
-                    </DialogContentText>
+                    {/* <DialogContentText id="alert-dialog-description"> */}
+                        <Typography variant="h5" className={classes.textTitle}>Hey {localStorage.getItem('name')}! Make sure you add our app to your homescreen!</Typography>
+                    {/* </DialogContentText> */}
                     <img alt="dark logo" src={addHomeGif} className={classes.badgeImg} />
-                    <DialogContentText id="alert-dialog-description" className={classes.textEntireBody}>
+                    {/* <DialogContentText id="alert-dialog-description" className={classes.textEntireBody}> */}
                         <Typography variant="body2" className={classes.textBody}>
                             <strong>For IOS devices: </strong> Make sure you're on Safari and then tap the Share
                             button at the bottom of your screen. Then tap on the "Add to Home
@@ -108,7 +108,7 @@ export default function DarkModeModal() {
                             <strong>For Andriod devices: </strong>In Chrome, go to Settings in the rop right
                             corner, then slide down and tap "Add to Home Screen".
                         </Typography>
-                    </DialogContentText>
+                    {/* </DialogContentText> */}
                     <Button
                         onClick={() => { handleClose(); setVisible(false); }}
                         variant="contained"
