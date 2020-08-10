b6c89deda4271c192af1754e4379816681b1f478
Katie Johnson <katiejohnson@Katies-MBP-4.fios-router.home>
2020-08-10 14:38:45 -0400

    (REFACTOR) Cleaned up spacing

--- src/pages/CompetePage/addToHomeScreenModal.js
+++ src/pages/CompetePage/addToHomeScreenModal.js
@@ -73,11 +73,11 @@ export default function DarkModeModal() {
         let addHomePop_done = JSON.parse(localStorage.getItem('addHomePop_done'));
         let email = localStorage.getItem('email');
         if (!addHomePop_done) {
-        // Displays modal and plays sound when it opens
-        setVisible(true);
-        playSound(badgeAudio);
-        // Tells firebase the action has been done
-        AddHomeOpened(email);
+            // Displays modal and plays sound when it opens
+            setVisible(true);
+            playSound(badgeAudio);
+            // Tells firebase the action has been done
+            AddHomeOpened(email);
         }
 
     }, [badgeAudio])
@@ -95,17 +95,17 @@ export default function DarkModeModal() {
                 {/* NOTE: dialogContent is styles in module.css, background wouldn't work otherwise */}
                 <DialogContent className={styles.dialogContentAddHome}>
                     {/* <DialogContentText id="alert-dialog-description"> */}
-                        <Typography variant="h5" className={classes.textTitle}>Hey {localStorage.getItem('name')}! Make sure you add our app to your homescreen!</Typography>
+                    <Typography variant="h5" className={classes.textTitle}>Hey {localStorage.getItem('name')}! Make sure you add our app to your homescreen!</Typography>
                     {/* </DialogContentText> */}
                     <img alt="dark logo" src={addHomeGif} className={classes.badgeImg} />
                     {/* <DialogContentText id="alert-dialog-description" className={classes.textEntireBody}> */}
-                        <Typography variant="body2" className={classes.textBody}>
-                            <strong>For IOS devices: </strong> Make sure you're on Safari and then tap the Share
+                    <Typography variant="body2" className={classes.textBody}>
+                        <strong>For IOS devices: </strong> Make sure you're on Safari and then tap the Share
                             button at the bottom of your screen. Then tap on the "Add to Home
                             Screen" button and our app will appear!
                         </Typography>
-                        <Typography variant="body2" className={classes.textBody}>
-                            <strong>For Andriod devices: </strong>In Chrome, go to Settings in the rop right
+                    <Typography variant="body2" className={classes.textBody}>
+                        <strong>For Andriod devices: </strong>In Chrome, go to Settings in the rop right
                             corner, then slide down and tap "Add to Home Screen".
                         </Typography>
                     {/* </DialogContentText> */}
