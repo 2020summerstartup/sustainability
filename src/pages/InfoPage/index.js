5131c98c3f520925e838f66ffdb4c4024ce7a23f
Amy Qian <aqian@g.hmc.edu>
2020-08-10 16:03:13 -0700

    refactor(infocard): delete unused files

--- src/pages/InfoPage/index.js
+++ src/pages/InfoPage/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import InfoCards from "./infoCards";
 import AddtoHomeScreen from "./addToHomeScreen";
-// import FBContactForm from "./fbContactForm";
 
 const InfoPage = () => (
   <div className="base-container">
-    {/* <PwaCard /> */}
     <InfoCards />
     <AddtoHomeScreen />
-    {/* <FBContactForm /> */}
   </div>
 );
 
